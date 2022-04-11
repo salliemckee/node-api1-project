b1e2fb02ea2981acfc4852e7e0c0188e708f14c3
Sallie McKee <salliemckee@Morrigan.local>
2022-04-11 17:18:52 -0500

    finished project

--- api/server.js
+++ api/server.js
@@ -62,6 +62,30 @@ server.delete("/api/users/:id", async (req, res) => {
   }
 });
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res
+          .status(400)
+          .json({ message: "Please provide name and bio for the user" });
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body);
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The user information could not be modified" });
+  }
+});
+
 server.use("*", (req, res) => {
   res.status(404).json({ message: "not found" });
 });
